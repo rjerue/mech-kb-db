f1f5abe454dc672288bddbba7c7ee67d52a61ea0
Ryan Jerue <ryan@jerue.org>
2021-09-04 20:48:46 -0400

    check if in vercel

--- pages/api/force-curve/[id].ts
+++ pages/api/force-curve/[id].ts
@@ -3,13 +3,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs/promises";
 import path from "path";
 
+const isVercel = typeof process.env.VERCEL !== "undefined";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any[]>
 ) {
   const { id } = req.query;
   const uuid = id as string;
-  const dir = path.resolve("./public", "force-curve", `${uuid}.json`);
+  const folderPath = isVercel ? ["./force-curve"] : ["./public", "force-curve"];
+  const dir = path.resolve(...folderPath.concat(`${uuid}.json`));
   const file = await fs.readFile(dir);
   const fileAsJson = JSON.parse(file.toString());
   res.status(200).json(fileAsJson);
